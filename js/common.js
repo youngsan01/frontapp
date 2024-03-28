92f3e347ffa01557163bf3a00fbd3ff8ff7fc68c
youngsan01 <intokys@gmail.com>
2024-03-28 12:51:47 +0900

    메인 페이지 작업중

---
+++ js/common.js
@@ -0,0 +1,11 @@
+window.addEventListener("DOMContentLoaded", function() {
+    const swiper = new Swiper(".mySwiper", {
+        navigation: {
+          nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev",
+        },
+        pagination: {
+            el: ".swiper-pagination",
+          },
+    });
+});
